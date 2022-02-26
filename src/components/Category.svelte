f057c7b416e30c2c38a6a908690da5ecfff7fe7d
simondoesstuff <sw.simonwalker@gmail.com>
2022-02-26 13:24:07 -0700

    nulls can now be passed through (category <-> assignment)

--- src/components/Category.svelte
+++ src/components/Category.svelte
@@ -3,14 +3,17 @@
 
 
     // todo set to undefined
-    export let gradeTarget = 90;             // used for 'backwards' calculation
+    // used for 'backwards' calculation
+    //      nullable -- (no course final grade target set)
+    export let gradeTarget;
 
     // the user writes these in
     export let title;
     export let weight = 1;
-    export let gradeOverride;
+    export let gradeOverride;   // nullable
 
     // the average of the contained assignments
+    //      nullable -- (assignments are null)
     export let gradeCalculated;
 
     let assignments = [
@@ -24,57 +27,78 @@
         }
     ];
 
-    // set the gradeCalculated based on assignments[]
+    // set the gradeCalculated based on an average of the assignments
     $: {
         let totalWeight = 0;
         let weightedGrades = 0;
 
         assignments.forEach(item => {
-            totalWeight += item.weight;
-
             // use the gradeTarget if the override isn't used
             let assignmentGrade = item.gradeOverride ?? item.gradeTarget;
 
+            // if neither was supplied, we are dealing with an empty assignment
+            //      skip it
+            if (!assignmentGrade) return;
+
+            totalWeight += item.weight;
             weightedGrades += item.weight * assignmentGrade;
         });
 
-        gradeCalculated = weightedGrades / totalWeight;
+        // if totalWeight is 0: either there are no assignments or they are all set to 0 weight
+        //      either way, the gradeCalculated can not be calculated --> set it to null
+        if (totalWeight === 0) {
+            gradeCalculated = null;
+        } else {
+            gradeCalculated = weightedGrades / totalWeight;
+        }
     }
 
     // set the assignments gradeTarget based on this.gradeTarget
     $: {
-        // todo not done yet
 
-        let totalWeight = 0;
-        let knownWeightedGrades = 0;
-        let unknownTotalWeight = 0;
+        // if there is no target grade for this category, the assignments dont get one either
 
-        // sum the total weight
-        assignments.forEach(item => {
-            totalWeight += item.weight;
-        });
+        if (gradeTarget == null) {
+            // apply calculated property to each assignment
+            assignments = assignments.map(item => {
+                item.gradeTarget = null;
+                return item;
+            });
+        } else {
 
-        // filter by assignments with an override
-        assignments
-            .filter(item => item.gradeOverride)
-            .forEach(item => {
-                knownWeightedGrades = item.weight * item.gradeOverride;
+            // backwards calculate each assignment's target grade
+
+            let totalWeight = 0;
+            let knownWeightedGrades = 0;
+            let unknownTotalWeight = 0;
+
+            // sum the total weight
+            assignments.forEach(item => {
+                totalWeight += item.weight;
             });
 
-        // filter by assignments without an override
-        assignments
-            .filter(item => !item.gradeOverride)
-            .forEach(item => {
-                unknownTotalWeight += item.weight;
+            // filter by assignments with an override
+            assignments
+                .filter(item => item.gradeOverride)
+                .forEach(item => {
+                    knownWeightedGrades = item.weight * item.gradeOverride;
+                });
+
+            // filter by assignments without an override
+            assignments
+                .filter(item => !item.gradeOverride)
+                .forEach(item => {
+                    unknownTotalWeight += item.weight;
+                });
+
+            let assignmentGradeTarget = ( gradeTarget * totalWeight - knownWeightedGrades ) / unknownTotalWeight;
+
+            // apply calculated property to each assignment
+            assignments = assignments.map(item => {
+                item.gradeTarget = assignmentGradeTarget;
+                return item;
             });
-
-        let assignmentGradeTarget = ( gradeTarget * totalWeight - knownWeightedGrades ) / unknownTotalWeight;
-
-        // apply calculated property to each assignment
-        assignments = assignments.map(item => {
-            item.gradeTarget = assignmentGradeTarget;
-            return item;
-        });
+        }
     }
 </script>
 
