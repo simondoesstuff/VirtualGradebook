311336506d64bc6b3d7eea2ee4b128f73300f7a9
simondoesstuff <sw.simonwalker@gmail.com>
2022-02-26 10:43:04 -0700

    implemented simple assignment and category components

---
+++ src/components/Assignment.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let gradeCalculated;
+
+    export let title;
+    export let weight = 1;
+    export let gradeOverride;
+</script>
+
+<!--    Title    -->
+<input value={title}>
+<!--    Weight    -->
+<input value={weight}>
+<!--    Grade   -->
+<input value={gradeOverride + "%"}>
---
+++ src/components/Category.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Assignment from "./Assignment.svelte";
+
+    export let gradeCalculated;
+
+    export let title;
+    export let weight = 1;
+    export let gradeOverride;
+
+    let assignments = [
+        {
+            title: "do your damn job",
+            gradeOverride: 50
+        }
+    ];
+</script>
+
+<!--    Title    -->
+<input value={title}>
+<!--    Weight    -->
+<input value={weight}>
+<!--    Grade   -->
+<input value={gradeOverride}>
+
+<div class="p-5">
+        {#each assignments as item}
+                <Assignment title={item.title} gradeOverride={item.gradeOverride} gradeCalculated={item.gradeCalculated}>
+                </Assignment>
+        {/each}
+</div>
