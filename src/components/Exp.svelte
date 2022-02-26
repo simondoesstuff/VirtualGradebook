88b4c766307809fab8d64a25ca31af0f09b81a7f
simondoesstuff <sw.simonwalker@gmail.com>
2022-02-26 10:14:13 -0700

    set up $components absolute path alias put in example usage

---
+++ src/components/Exp.svelte
@@ -0,0 +1,7 @@
+
+
+<div class="grid place-items-center">
+    <h5 class="bg-indigo-500 rounded-2xl p-3 text-white font-bold">
+        What up Zillard?
+    </h5>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,11 @@
+<script>
+    import Exp from "$components/Exp.svelte";
+</script>
+
 <h1 class="text-3xl font-bold underline">
     Hello world!
 </h1>
+
+<div class="m-5">
+    <Exp/>
+</div>
