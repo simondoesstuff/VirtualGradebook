91327b91fda108b6145310d1198914c63cd118cb
simondoesstuff <sw.simonwalker@gmail.com>
2022-02-27 00:21:31 -0700

    added state to index.svelte to hold multiple courses

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,17 +3,29 @@
     import {uniqID} from "$scripts/Utils.ts";
 
 
-    let courses = [
-        {
-            title: "CSC244",
-            categories: [
-                {assignments: [
-                    {id: uniqID()}
-                ], id: uniqID()},
-            ],
-            id: uniqID()
-        }
-    ];
+    let courses = [];
+
+    function removeCourse(catIndex) {
+        courses.splice(catIndex, 1);
+        courses = courses
+    }
+
+    function addDefaultCourse() {
+        courses = [
+            ...courses,
+            {
+                categories: [
+                    {assignments: [
+                            {id: uniqID()}
+                        ], id: uniqID()},
+                ],
+                id: uniqID()
+            }
+        ]
+    }
+
+    // add an inital entry to the state
+    addDefaultCourse();
 </script>
 
 {#each courses as course (course.id)}
