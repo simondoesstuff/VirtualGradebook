396f5a184963a8440b5a3d5eb38a7eb67c31697b
Gage Hundertmark <hundertmarkgage@gmail.com>
2022-03-11 21:42:55 -0700

    sauper good now

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import pytest
+from contextlib import redirect_stdout
 
-print(pytest.main())
+with redirect_stdout(None):
+
+# print(pytest.main(["test_crap.py::test_capital_case"]))
+
+print(
+    pytest.main(["--co"])
+)
